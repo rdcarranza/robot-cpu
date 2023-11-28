66b89ed050332bbb79ef9fc2960129fa6b7223d3
Dario Carranza <dario_zmd@yahoo.com.ar>
2023-11-27 23:28:11 -0300

    Modificación en el robot.

--- robot.sh
+++ robot.sh
@@ -6,7 +6,15 @@
 sh ./grabar.sh
 
 #   si se creó el archivo ingresovoz.mp3 continua, caso contrario se interrumpe.
+arch1=./ingresovoz.mp3
 
+if [-f "$arch1"]
+then
+echo "ingresovoz.mp3 se generó correctamente!"
+else
+exit ;
+echo "Error en la captura de audio."
+fi
 
 #VOZATEXTO-IA. GO.
 #Se convierte ingresovoz.mp3 en texto plano, usando una API AI.
