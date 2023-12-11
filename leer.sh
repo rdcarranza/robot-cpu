3cb7870fc450b749fa68907a55ae1fa04599715f
Dario Carranza <dario_zmd@yahoo.com.ar>
2023-12-11 13:28:52 -0300

    pequeña correción.
    Agrego la rama v0.1

--- leer.sh
+++ leer.sh
@@ -1,3 +1,4 @@
 arch=./ingresotexto.txt
 
 #lectura del archivo.
+#espeak -g 10 -p 20 -v "spanish-latin-am" "texto"
--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+package robot-cpu
 
 import (
 	"rdcarranza/consulta_ia"
